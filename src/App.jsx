7d0f811107c9f2259f621223250efa8f8903986d
HOELCODE <hcostantini1@gmail.com>
2025-03-25 16:26:22 +0100

    nav et aside et main ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import Navbar from './layout/Navbar';
 import Aside from './layout/Aside';
+import Main from './layout/Main';
+
+import './css/App.css';
 
 function App() {
 
   return (
     <div className="App">
       <Navbar />
-      <Aside />
+      <div className="container">
+        <Aside />
+        <Main />
+      </div>
+
     </div>
   )
 }
