4c2a9cde0701776324303659a78c4432b9cf564b
HOELCODE <hcostantini1@gmail.com>
2025-03-29 16:37:00 +0100

    encore quelques modfis de graphique

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -13,7 +13,7 @@ const Activity = ({ data }) => {
     // Définir les dimensions du graphique
     const width = 600;
     const height = 400;
-    const margin = { top: 20, right: 30, bottom: 50, left: 40 };
+    const margin = { top: 20, right: 40, bottom: 50, left: 40 };
 
     // Supprimer l'ancien SVG s'il existe
     d3.select(chartRef.current).selectAll("*").remove();
@@ -24,12 +24,13 @@ const Activity = ({ data }) => {
       .attr("width", width)
       .attr("height", height);
 
-    // Échelles
+    // Échelle X
     const xScale = d3.scaleBand()
-      .domain(sortedData.map((_, i) => i + 1)) // 1,2,3...
+      .domain(sortedData.map((_, i) => i + 1))
       .range([margin.left, width - margin.right])
-      .padding(0.65);
+      .paddingInner(0.5); // Équilibre les espaces entre les barres
 
+    // Échelle Y
     const yScale = d3.scaleLinear()
       .domain([d3.min(sortedData, d => d.kilogram) - 2, d3.max(sortedData, d => d.kilogram)])
       .range([height - margin.bottom, margin.top]);
@@ -43,7 +44,7 @@ const Activity = ({ data }) => {
       .attr("x", (_, i) => xScale(i + 1))
       .attr("y", d => yScale(d.kilogram))
       .attr("height", d => height - margin.bottom - yScale(d.kilogram))
-      .attr("width", "7px")
+      .attr("width", "7px") // Ajustement pour bien répartir les barres
       .attr("fill", "#282D30");
 
     // Ajouter les barres des calories
@@ -52,7 +53,7 @@ const Activity = ({ data }) => {
       .enter()
       .append("rect")
       .attr("class", "bar-calories")
-      .attr("x", (_, i) => xScale(i + 1) + 15)
+      .attr("x", (_, i) => xScale(i + 1) + xScale.bandwidth() / 2)
       .attr("y", d => yScale(d.kilogram))
       .attr("height", d => height - margin.bottom - yScale(d.kilogram))
       .attr("width", "7px")
@@ -61,12 +62,33 @@ const Activity = ({ data }) => {
     // Ajouter l'axe X
     svg.append("g")
       .attr("transform", `translate(0,${height - margin.bottom})`)
-      .call(d3.axisBottom(xScale).tickFormat(d => d));
+      .call(d3.axisBottom(xScale).tickFormat(d => d))
+      .call(d3.axisBottom(xScale).tickSize(0));
 
-    // Ajouter l'axe Y
+    // Personnalisation de l'axe X
+    svg.selectAll(".tick text")
+      .attr("x", "-8px")
+      .attr("fill", "#9B9EAC")
+      .attr("y", "20px")
+      .style("font-size", "14px")
+
+    svg.selectAll(".tick line").remove();
+    svg.selectAll(".domain").attr("stroke", "#9B9EAC");
+    svg.selectAll(".tick text")
+
+
+    // Ajouter l'axe Y à droite
     svg.append("g")
-      .attr("transform", `translate(${width - margin.right},0)`) // Déplace l'axe Y à droite
-      .call(d3.axisRight(yScale)); // Change la direction des ticks
+      .attr("transform", `translate(${width - margin.right},0)`)
+      .call(d3.axisRight(yScale).tickSize(0));
+
+    // Personnalisation de l'axe Y
+    svg.selectAll(".tick line").remove();
+    svg.selectAll("g:nth-of-type(2) .domain").remove();
+    svg.selectAll(".tick text")
+      .attr("fill", "#9B9EAC")
+      .style("font-size", "14px")
+      .attr("x", "20px")
 
 
   }, [data]);
